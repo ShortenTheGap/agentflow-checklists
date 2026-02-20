bd5d755e59b08ba83acf5f7c9cd468c0b41335ef
Gus Van Dender <gus@shortenthegap.com>
2026-02-20 04:15:35 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,23 @@
+import React, { useEffect } from "react";
+import { base44 } from "@/api/base44Client";
+import { createPageUrl } from "@/utils";
+
 export default function Home() {
+  useEffect(() => {
+    const redirect = async () => {
+      try {
+        const user = await base44.auth.me();
+        if (user?.role === "admin") {
+          window.location.href = createPageUrl("AdminDashboard");
+        } else {
+          window.location.href = createPageUrl("AgentDashboard");
+        }
+      } catch {
+        // Not authenticated, platform will show login
+      }
+    };
+    redirect();
+  }, []);
+
   return null;
 }
