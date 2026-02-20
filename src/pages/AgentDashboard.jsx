e644238af4db3c28dc1c30201ecf6177e369dba2
Gus Van Dender <gus@shortenthegap.com>
2026-02-20 04:08:49 +0000

    File changes

--- src/pages/AgentDashboard.jsx
+++ src/pages/AgentDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { base44 } from "@/api/base44Client";
+import { createPageUrl } from "@/utils";
 import StatusBadge from "@/components/StatusBadge";
 import { Clock, Pencil, Send, LogOut } from "lucide-react";
 import { Button } from "@/components/ui/button";
